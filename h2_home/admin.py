d666085597fd5e5621853d82e8b7ff736c5fdcc6
xue947441187 <947441187@qq.com>
2018-08-29 10:18:17 +0800

    第二天上午更新：增加了背景图片，以及一个css框架，首页设计正在进行中

--- h2_home/admin.py
+++ h2_home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserInfo,LabelInfo,ObjInfo
+from .models import UserInfo, LabelInfo, ObjInfo
+
 # Register your models here.
 
 admin.site.site_header = "郑工15市2后台管理"
@@ -9,17 +10,17 @@ admin.site.site_title = "市2后台管理"
 @admin.register(UserInfo)
 class UserInfoAdmin(admin.ModelAdmin):
     empty_value_display = "none"
-    list_display = ("id","name","age","gender","height","email","phone","qq_howl","single","is_delete")
-    exclude = ("value1","value2","value3")
+    list_display = ("id", "name", "age", "gender", "height", "email", "phone", "qq_howl", "single", "is_delete")
+    exclude = ("value1", "value2", "value3")
 
 
 @admin.register(LabelInfo)
 class LabelInfoAdmin(admin.ModelAdmin):
     empty_value_display = "none"
-    list_display = ("title","count")
+    list_display = ("title", "count")
+
 
 @admin.register(ObjInfo)
 class ObjInfoAdmin(admin.ModelAdmin):
     empty_value_display = "none"
-    list_display = ("user_id","age","name","gender","height","weight","email","phone","qq_howl")
-
+    list_display = ("user_id", "age", "name", "gender", "height", "weight", "email", "phone", "qq_howl")
--- h2_home/views.py
+++ h2_home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context ={
+        "title":'首页'
+    }
+    return render(request,"h2_home/index.html",context)
